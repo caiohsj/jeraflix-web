0daf30ab7f2087e4e5ab7f23d900c5388970fa24
Caio Henrique <caiocoxim10@gmail.com>
2021-02-12 22:58:18 -0400

    [FIX] Carregando headers por meio de uma funçao

--- src/services/jeraflix_api/watched_movie/index.js
+++ src/services/jeraflix_api/watched_movie/index.js
@@ -1,7 +1,8 @@
-import {http, headers} from '../config.js'
+import {http, getHeaders} from '../config.js'
 
 export default {
     add(profile, movie) {
+        const headers = getHeaders()
         return http.post('/watched_movie', {
             profile_id: profile.id,
             movie_id: movie.id
